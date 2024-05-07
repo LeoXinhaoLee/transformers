bad0805d91a3501b158012296f6d6b75ea7e49d4
LeoXinhaoLee <Mc1114207918@outlook.com>
2024-05-06 18:31:13 -0700

    change pythia to llama in comment

--- benchmark_prefill_decode.py
+++ benchmark_prefill_decode.py
@@ -48,7 +48,7 @@ from transformers.models.ttt_benchmark_cg.configuration_ttt import TTT_STANDARD_
 parser = argparse.ArgumentParser(description="Generation benchmarking")
 parser.add_argument("--logdir", type=str, default="./exp/clean")
 parser.add_argument("--model-name", type=str, default="openai-community/gpt2")
-# state-spaces/mamba-130m | EleutherAI/pythia-1.4b | state-spaces/mamba-1.4b | ttt-125m | ttt-1b | ttt-profile
+# state-spaces/mamba-130m | meta-llama/Llama-2-7b | state-spaces/mamba-1.4b | ttt-125m | ttt-1b | ttt-profile
 parser.add_argument("--mode", type=str, default="prefill", choices=["prefill", "decode"])
 parser.add_argument("--promptlen", type=int, default=1)
 parser.add_argument("--genlen", type=int, default=128)
@@ -127,7 +127,7 @@ else:
     #                                              device_map={"": device},
     #                                              torch_dtype=dtype)
     tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-neox-20b")  # meta-llama/Llama-2-7b
-    config = LlamaConfig.from_json_file('./llama_config/config.json')
+    config = LlamaConfig.from_json_file('./llama_config/config.json')  # 1B llama config
     config._attn_implementation = args.attn_impl  # @xinhao: llama config use `_attn_implementation` to select attn
     model = LlamaForCausalLM(config).to(device=device, dtype=dtype)
 
